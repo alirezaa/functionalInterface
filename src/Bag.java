b42c833a9f335e81fd397298f4b0ede4350a358f
alirezaa <aghamoa@yahoo.com>
2015-07-12 22:25:53 -0700

    added .gitignore

---
+++ src/Bag.java
@@ -0,0 +1,28 @@
+
+//very simple java 8 example
+//Alireza Aghamohammadi
+public class Bag {
+
+	public interface Items {
+		int value(int currentItem);
+	}
+
+	public Items find() {
+		//anonymous inner class
+		//	    return new Items() {
+		//	     @Override
+		//	      public int value(int currentValue) {
+		//	    	 return ++currentValue; }
+		//	    };  
+
+		//the below works fine isn't this less code ! :)
+		//typical format also works ( some parameter ) -> { body }
+		return currentItem -> ++currentItem; 
+	}
+	public static void main(String[] args) {
+		Bag p = new Bag();
+		int ourCurrentIteminBox=11;
+		System.out.println("Next item in the Bag is: "+p.find().value(ourCurrentIteminBox));
+
+	}
+}
